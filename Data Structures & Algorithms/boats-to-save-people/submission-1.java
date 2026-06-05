f61a466fd421a9aa5b9387de84166e95fb24f176
peropranav-1 <pranavkapoor99@gmail.com>
2026-06-05 23:36:40 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length - 1;
+        int boatCount = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            boatCount++;
+        }
+        return boatCount;
+    }
+}
